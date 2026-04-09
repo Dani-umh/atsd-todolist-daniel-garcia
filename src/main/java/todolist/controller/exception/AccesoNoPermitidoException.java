5f7290f5b6f06885d91c05c88da8a175e9b0631c
Daniel Garcia Moreno <daniel.garcia34@goumh.umh.es>
2026-04-09 18:13:44 +0200

    Add unauthorized exception for admin-only user pages

---
+++ src/main/java/todolist/controller/exception/AccesoNoPermitidoException.java
@@ -0,0 +1,8 @@
+package todolist.controller.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "Permisos insuficientes")
+public class AccesoNoPermitidoException extends RuntimeException {
+}
