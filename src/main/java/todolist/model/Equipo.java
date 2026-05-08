ab5890e97d9c464865cda0aac002de2f9a076af2
Daniel Garcia Moreno <daniel.garcia34@goumh.umh.es>
2026-05-08 17:14:55 +0200

    Second test - Adding and searching for teams in the db

--- src/main/java/todolist/model/Equipo.java
+++ src/main/java/todolist/model/Equipo.java
@@ -1,11 +1,31 @@
 package todolist.model;
 
-public class Equipo {
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
+@Entity
+@Table(name = "equipos")
+public class Equipo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
     private String nombre;
+
+    public Equipo() {
+    }
 
     public Equipo(String nombre) {
         this.nombre = nombre;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getNombre() {
