5c97b0a36da81e0a7ee0b4c0755b066e3592c0f6
Daniel Garcia Moreno <daniel.garcia34@goumh.umh.es>
2026-05-12 12:37:55 +0200

    Add join and leave team actions

--- src/main/java/todolist/controller/EquipoController.java
+++ src/main/java/todolist/controller/EquipoController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import todolist.authentication.ManagerUserSession;
 import todolist.dto.EquipoData;
 import todolist.dto.UsuarioData;
@@ -31,9 +32,11 @@ public class EquipoController {
 
         UsuarioData usuario = usuarioService.findById(idUsuario);
         List<EquipoData> equipos = equipoService.findAllOrdenadoPorNombre();
+        List<EquipoData> equiposUsuario = equipoService.equiposUsuario(idUsuario);
 
         model.addAttribute("usuario", usuario);
         model.addAttribute("equipos", equipos);
+        model.addAttribute("equiposUsuario", equiposUsuario);
 
         return "listaEquipos";
     }
@@ -51,5 +54,19 @@ public class EquipoController {
         model.addAttribute("usuarios", usuarios);
 
         return "usuariosEquipo";
+    }
+
+    @PostMapping("/equipos/{id}/unirse")
+    public String unirseEquipo(@PathVariable(value = "id") Long idEquipo) {
+        Long idUsuario = managerUserSession.usuarioLogeado();
+        equipoService.añadirUsuarioAEquipo(idEquipo, idUsuario);
+        return "redirect:/equipos";
+    }
+
+    @PostMapping("/equipos/{id}/salir")
+    public String salirEquipo(@PathVariable(value = "id") Long idEquipo) {
+        Long idUsuario = managerUserSession.usuarioLogeado();
+        equipoService.eliminarUsuarioDeEquipo(idEquipo, idUsuario);
+        return "redirect:/equipos";
     }
 }
